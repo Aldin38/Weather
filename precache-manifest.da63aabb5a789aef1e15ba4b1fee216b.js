67c1f098ae6bed34c55ed5c34f01448e20ad9fd1
Aldin38 <aldin_mikara@hotmail.com>
2020-06-09 21:47:04 +0200

    Updates

---
+++ precache-manifest.da63aabb5a789aef1e15ba4b1fee216b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f723823a6b40afe34c162e41b376a300",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "374496b286b7834f8a54",
+    "url": "/Weather/static/css/main.ca7939e6.chunk.css"
+  },
+  {
+    "revision": "5c2590b55bff6cd01ccb",
+    "url": "/Weather/static/js/2.8e47e1d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.8e47e1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374496b286b7834f8a54",
+    "url": "/Weather/static/js/main.ffb7005a.chunk.js"
+  },
+  {
+    "revision": "0356e636dd9d77987dda",
+    "url": "/Weather/static/js/runtime-main.ab064488.js"
+  },
+  {
+    "revision": "95f5f688759be297c328cdd4609ea48a",
+    "url": "/Weather/static/media/desktop-clouds-day.95f5f688.jpg"
+  },
+  {
+    "revision": "8363820b5fa43d1a11304b1937da6f16",
+    "url": "/Weather/static/media/desktop-clouds-night.8363820b.jpg"
+  },
+  {
+    "revision": "9388a0bdd2901175fe2d480c9c013de6",
+    "url": "/Weather/static/media/desktop-rain-day.9388a0bd.jpg"
+  },
+  {
+    "revision": "eefa9d8a0c944199897d076ae02acffd",
+    "url": "/Weather/static/media/desktop-rain-night.eefa9d8a.jpg"
+  },
+  {
+    "revision": "b0e9a969def3132d2dd713fbff7afe75",
+    "url": "/Weather/static/media/desktop-snow.b0e9a969.jpg"
+  },
+  {
+    "revision": "96d8a25981835fe9448c48804eceaf6f",
+    "url": "/Weather/static/media/desktop-sunny-day.96d8a259.jpg"
+  },
+  {
+    "revision": "9159612d20d439ac85620a97c192c784",
+    "url": "/Weather/static/media/desktop-thunder-tain-day.9159612d.jpg"
+  },
+  {
+    "revision": "bff65a17a8b338ea7597871f17712b98",
+    "url": "/Weather/static/media/mobile-clear-night.bff65a17.jpg"
+  },
+  {
+    "revision": "6755fd1038ed549eba19e6e5a4c5f67b",
+    "url": "/Weather/static/media/mobile-clouds-night.6755fd10.jpg"
+  },
+  {
+    "revision": "653a62ddde39cff4317312f01a9878d0",
+    "url": "/Weather/static/media/mobile-rain-night.653a62dd.jpg"
+  },
+  {
+    "revision": "624b0fce58f97ebc0684c2240b8940fa",
+    "url": "/Weather/static/media/mobile-snow-night.624b0fce.jpg"
+  },
+  {
+    "revision": "f3e800a4018476e99b5185ad1ef9fdc7",
+    "url": "/Weather/static/media/rain-weather.f3e800a4.jpg"
+  },
+  {
+    "revision": "f1141cffdc8159f08f3add46841453bf",
+    "url": "/Weather/static/media/snow-desktop-night.f1141cff.jpg"
+  },
+  {
+    "revision": "6b615c321c864552ad8b98a42c501e1d",
+    "url": "/Weather/static/media/snowy-weather.6b615c32.jpg"
+  },
+  {
+    "revision": "1b0584b2466623bfefbc1f5a4b605914",
+    "url": "/Weather/static/media/sunny-weather-suljo.1b0584b2.jpg"
+  },
+  {
+    "revision": "88a31020f8bee957fc9d68f81a08fbd2",
+    "url": "/Weather/static/media/sunny-weather.88a31020.jpg"
+  },
+  {
+    "revision": "61f8ccc484f5714c29e88b9f65ab77fb",
+    "url": "/Weather/static/media/thunder-and-rain.61f8ccc4.jpg"
+  },
+  {
+    "revision": "fdefeb522053f086a8ce420f85d0c145",
+    "url": "/Weather/static/media/thunder-weather.fdefeb52.jpg"
+  }
+]);
